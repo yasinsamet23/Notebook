959896e7279c44dbfc7ee6b5041b0643d954132e
yasin <yasin@MSI>
2025-03-03 02:21:05 +0300

    Add project files.

---
+++ Notebook/Form1.cs
@@ -0,0 +1,576 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notebook
+{
+    public partial class Form1 : Form
+    {
+
+        private int TabCount = 0;   //Tab Count
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region Methods
+        #region Tabs
+        private void AddTab()
+        {
+            RichTextBox Body = new RichTextBox();
+            Body.Name = "Body";
+            Body.Dock = DockStyle.Fill;
+            Body.ContextMenuStrip = contextMenuStrip1;
+
+            TabPage NewPage= new TabPage();
+            TabCount += 1;
+            string DocumentText= "Document" + TabCount;
+            NewPage.Name = DocumentText;
+            NewPage.Text= DocumentText;
+            NewPage.Controls.Add( Body );
+            
+            tabControl1.TabPages.Add( NewPage );
+        
+        }
+        private void RemoveTab()
+        {
+            if(tabControl1.TabPages.Count!=1)
+            {
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+            }
+            else
+            {
+                tabControl1.TabPages.Remove(tabControl1.SelectedTab);
+                AddTab();
+            }
+
+        }
+
+        private void RemoveAllTabs()
+        {
+            foreach(TabPage Page in tabControl1.TabPages)
+            {
+                tabControl1.TabPages.Remove(Page);
+            }
+            AddTab();
+        }
+
+        private void RemoveAllTabsButThis()     // Close all tabs except the open one
+        {
+            foreach(TabPage Page in tabControl1.TabPages)
+            {
+                if(Page.Name!=tabControl1.SelectedTab.Name)
+                {
+                    tabControl1.TabPages.Remove(Page);
+                }
+            }
+        }
+
+
+
+        #endregion
+
+        #region SaveOpen
+        private void Save()
+        {
+            // If the file has been saved before (FileName exists), it will be overwritten.
+            if (string.IsNullOrEmpty(saveFileDialog1.FileName))
+            {
+                // If the file has not been saved before, the "Save As" window will open.
+                SaveAs();
+            }
+            else
+            {
+                try
+                {
+                    // Saving is done according to the file extension
+                    string extension = System.IO.Path.GetExtension(saveFileDialog1.FileName).ToLower();
+                    if (extension == ".rtf")
+                    {
+                        GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                    }
+                    else if (extension == ".txt")
+                    {
+                        GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                    }
+                    else if (extension == ".cs")
+                    {
+                        GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText); // C# file is saved as plain text
+                    }
+                    else
+                    {
+                        MessageBox.Show("Unsupported file type.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"File could not be saved: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                }
+            }
+        }
+
+
+
+        private void SaveAs()
+        {
+            saveFileDialog1.FileName = tabControl1.SelectedTab.Name;
+            saveFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // We are showing the user which file types they can select
+            saveFileDialog1.Filter = "RTF Files|*.rtf|Text Files|*.txt|C# Files|*.cs|All Files|*.*";
+            saveFileDialog1.Title = "Save As";
+
+            // If the user does not select a file or cancels, the FileName is reset.
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if (saveFileDialog1.FileName.Length > 0)
+                {
+                    try
+                    {
+                        // Saving is done according to the file extension
+                        string extension = System.IO.Path.GetExtension(saveFileDialog1.FileName).ToLower();
+                        if (extension == ".rtf")
+                        {
+                            GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                        }
+                        else if (extension == ".txt")
+                        {
+                            GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                        }
+                        else if (extension == ".cs")
+                        {
+                            GetCurrentDocument.SaveFile(saveFileDialog1.FileName, RichTextBoxStreamType.PlainText); // C# file is saved as plain text
+                        }
+                        else
+                        {
+                            MessageBox.Show("Unsupported file type.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"File could not be saved: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            else
+            {
+                // If the user presses "Cancel" the FileName will be reset
+                saveFileDialog1.FileName = string.Empty;
+            }
+        }
+
+
+
+        
+
+
+
+        private void Open()
+        {
+            openFileDialog1.InitialDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            openFileDialog1.Filter = "RTF|*.rtf|Text File|*.txt|C# File|*.cs|All Files|*.*";
+
+            if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                if (openFileDialog1.FileName.Length > 0)
+                {
+                    try
+                    {
+                        // We check the file extension
+                        string fileExtension = System.IO.Path.GetExtension(openFileDialog1.FileName).ToLower();
+
+                        if (fileExtension == ".rtf")
+                        {
+                            GetCurrentDocument.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.RichText);
+                        }
+                        else if (fileExtension == ".txt" || fileExtension == ".cs")
+                        {
+                            // Upload .txt and .cs files as plain text
+                            GetCurrentDocument.LoadFile(openFileDialog1.FileName, RichTextBoxStreamType.PlainText);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Unsupported file type.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"File could not be opened: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                    }
+                }
+            }
+        }
+
+
+
+
+        #endregion
+
+        #region
+        private RichTextBox GetCurrentDocument  // Get Active Document
+        {
+            get { return (RichTextBox)tabControl1.SelectedTab.Controls["Body"]; }
+
+        }
+
+        #endregion TextFunctions
+
+        #region TextFunctions
+        private void Undo()
+        {
+            GetCurrentDocument.Undo();
+        }
+        private void Redo()
+        {
+            GetCurrentDocument.Redo();
+        }
+        private void Cut()
+        {
+            GetCurrentDocument.Cut();
+        }
+        
+        private void Copy()
+        {
+            GetCurrentDocument.Copy();
+        }
+        private void Paste()
+        {
+            GetCurrentDocument.Paste();
+        }
+        private void SelectAll()
+        {
+            GetCurrentDocument.SelectAll();
+        }
+
+
+        #endregion
+        #region Font
+
+        private void GetFontCollection()   // Loading fonts
+        {
+            InstalledFontCollection InsFonts = new InstalledFontCollection();
+            foreach(FontFamily item in InsFonts.Families)
+            {
+                toolStripComboBoxFontType.Items.Add(item.Name);
+            }
+            toolStripComboBoxFontType.SelectedIndex = 0;
+
+        }
+
+        private void PopulateFontSize()    // Create font scales
+        {
+            for(int i=1; i<=75; i++)
+            {
+                toolStripComboBoxFontSize.Items.Add(i);
+            }
+            toolStripComboBoxFontSize.SelectedIndex = 12;
+
+
+        }
+
+        #endregion
+
+        #endregion
+
+        private void undoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+
+        private void redoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Redo();
+        }
+
+        private void cutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void copyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void pasteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void selectAllToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SelectAll();
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AddTab();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void saveAsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveAs();
+        }
+
+        private void newToolStripButton_Click(object sender, EventArgs e)
+        {
+            AddTab();
+        }
+
+        private void removetoolStripButton1_Click(object sender, EventArgs e)
+        {
+            RemoveTab();
+        }
+
+        private void openToolStripButton_Click(object sender, EventArgs e)
+        {
+            Open();
+        }
+
+        private void saveToolStripButton_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void cutToolStripButton_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void copyToolStripButton_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void pasteToolStripButton_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void toolStripButtonBold_Click(object sender, EventArgs e)
+        {
+            Font BoldFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+                GetCurrentDocument.SelectionFont.SizeInPoints,FontStyle.Bold);
+
+            Font RegularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+               GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            if(GetCurrentDocument.SelectionFont.Bold)
+            {
+                GetCurrentDocument.SelectionFont = RegularFont; 
+            }
+            else
+            {
+                GetCurrentDocument.SelectionFont = BoldFont;
+            }
+        
+        }
+
+        private void toolStripButtonItalic_Click(object sender, EventArgs e)
+        {
+            Font ItalicFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+               GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Italic);
+
+            Font RegularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+               GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            if (GetCurrentDocument.SelectionFont.Italic)
+            {
+                GetCurrentDocument.SelectionFont = RegularFont;
+            }
+            else
+            {
+                GetCurrentDocument.SelectionFont = ItalicFont;
+            }
+        }
+
+        private void toolStripButtonUnderline_Click(object sender, EventArgs e)
+        {
+            Font UnderlineFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+               GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Underline);
+
+            Font RegularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+               GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            if (GetCurrentDocument.SelectionFont.Underline)
+            {
+                GetCurrentDocument.SelectionFont = RegularFont;
+            }
+            else
+            {
+                GetCurrentDocument.SelectionFont = UnderlineFont;
+            }
+        }
+
+        private void toolStripButtonStrikeout_Click(object sender, EventArgs e)
+        {
+            Font StrikeoutFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+               GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Strikeout);
+
+            Font RegularFont = new Font(GetCurrentDocument.SelectionFont.FontFamily,
+               GetCurrentDocument.SelectionFont.SizeInPoints, FontStyle.Regular);
+
+            if (GetCurrentDocument.SelectionFont.Strikeout)
+            {
+                GetCurrentDocument.SelectionFont = RegularFont;
+            }
+            else
+            {
+                GetCurrentDocument.SelectionFont = StrikeoutFont;
+            }
+        }
+
+        private void toolStripButtonUpper_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectedText = GetCurrentDocument.SelectedText.ToUpper();
+        }
+
+        private void toolStripButtonLower_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectedText = GetCurrentDocument.SelectedText.ToLower();
+        }
+
+        private void toolStripButtonIncrease_Click(object sender, EventArgs e)
+        {
+            float NewFontSize = GetCurrentDocument.SelectionFont.SizeInPoints + 2;
+            Font NewSize = new Font(GetCurrentDocument.SelectionFont.Name, 
+                NewFontSize,GetCurrentDocument.SelectionFont.Style);
+            GetCurrentDocument.SelectionFont = NewSize;
+        }
+
+        private void toolStripButtonDecrease_Click(object sender, EventArgs e)
+        {
+            float NewFontSize = GetCurrentDocument.SelectionFont.SizeInPoints - 2;
+            Font NewSize = new Font(GetCurrentDocument.SelectionFont.Name,
+                NewFontSize, GetCurrentDocument.SelectionFont.Style);
+            GetCurrentDocument.SelectionFont = NewSize;
+        }
+
+        private void toolStripButtonFontColor_Click(object sender, EventArgs e)
+        {
+            if(colorDialog1.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+            {
+                GetCurrentDocument.SelectionColor=colorDialog1.Color;
+            }
+        }
+
+        private void toolStripMenuItemGreen_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectionBackColor=Color.Lime;
+        }
+
+        private void toolStripMenuItemOrange_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectionBackColor = Color.DarkOrange;
+        }
+
+        private void toolStripMenuItemYellow_Click(object sender, EventArgs e)
+        {
+            GetCurrentDocument.SelectionBackColor = Color.Gold;
+        }
+
+        private void toolStripComboBoxFontType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Font NewFont = new Font(toolStripComboBoxFontType.SelectedItem.ToString(),
+                GetCurrentDocument.SelectionFont.Size, 
+                GetCurrentDocument.SelectionFont.Style);
+            GetCurrentDocument.SelectionFont = NewFont;
+        }
+
+        private void toolStripComboBoxFontSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            float NewSize;
+            float.TryParse(toolStripComboBoxFontSize.SelectedItem.ToString(),
+                out NewSize);
+            Font NewFont = new Font(
+                GetCurrentDocument.SelectionFont.Name,
+                NewSize,
+                GetCurrentDocument.SelectionFont.Style);
+            GetCurrentDocument.SelectionFont = NewFont;
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Undo();
+        }
+
+        private void toolStripMenuItem2_Click(object sender, EventArgs e)
+        {
+            Redo();
+        }
+
+        private void toolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+            Cut();
+        }
+
+        private void toolStripMenuItem4_Click(object sender, EventArgs e)
+        {
+            Copy();
+        }
+
+        private void toolStripMenuItem5_Click(object sender, EventArgs e)
+        {
+            Paste();
+        }
+
+        private void toolStripMenuItem6_Click(object sender, EventArgs e)
+        {
+            Save();
+        }
+
+        private void toolStripMenuItem7_Click(object sender, EventArgs e)
+        {
+            RemoveTab();
+        }
+
+        private void toolStripMenuItem8_Click(object sender, EventArgs e)
+        {
+            RemoveAllTabs();
+        }
+
+        private void toolStripMenuItem9_Click(object sender, EventArgs e)
+        {
+            RemoveAllTabsButThis();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            AddTab();
+            GetFontCollection();
+            PopulateFontSize();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if(GetCurrentDocument.Text.Length > 0)
+            {
+                toolStripStatusLabel1.Text = "Total number of characters = " +
+                    GetCurrentDocument.Text.Length.ToString();
+            }
+
+        }
+    }
+}
